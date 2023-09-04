e5f2bd27a2b450a07124ed1f45dde16b91e82b40
pipaaa <86318578+pipaaa@users.noreply.github.com>
2023-09-04 19:59:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 // script.js
-// Puedes agregar aquí tus animaciones e interacciones con el usuario.
+
+// Función para ocultar el logo de carga cuando la página esté completamente cargada
+window.addEventListener('load', () => {
+    const loadingLogo = document.getElementById('loading-logo');
+    if (loadingLogo) {
+        loadingLogo.style.display = 'none';
+    }
+});
