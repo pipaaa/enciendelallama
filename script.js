f686440d27cbacdc15f2257747046c3e60d168e8
pipaaa <86318578+pipaaa@users.noreply.github.com>
2023-09-04 20:04:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,13 +9,17 @@ window.addEventListener('load', () => {
 });
 // script.js
 
-// Función para ocultar el logo de carga después de 5 segundos
-window.addEventListener('load', () => {
+// Función para mostrar el logo de carga y luego ocultarlo después de 5 segundos
+document.addEventListener('DOMContentLoaded', () => {
     const loadingLogo = document.getElementById('loading-logo');
-    if (loadingLogo) {
-        setTimeout(() => {
-            loadingLogo.style.display = 'none';
-        }, 5000); // 5000 milisegundos (5 segundos)
-    }
-});
 
+    // Muestra el logo de carga al principio
+    loadingLogo.style.display = 'block';
+
+    // Después de 5 segundos, oculta el logo
+    setTimeout(() => {
+        loadingLogo.style.display = 'none';
+        
+        // Agrega aquí cualquier otra lógica que desees ejecutar después de que el logo se haya ocultado
+    }, 5000); // 5000 milisegundos (5 segundos)
+});
